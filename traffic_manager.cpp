464094f65a5153a7f6b8111a40b91bdbd21c19c7
niki4smirn <kitos.smirnov@gmail.com>
2022-02-13 19:13:04 +0300

    Add TrafficManager interface

---
+++ traffic_manager.cpp
@@ -0,0 +1,1 @@
+#include "traffic_manager.h"
---
+++ traffic_manager.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <vector>
+
+class Graph;
+
+class TrafficManager {
+ public:
+  TrafficManager(
+      const Graph& graph,
+      std::vector<int> buns_amounts,
+      std::vector<int> vehicles,
+      int vehicle_capacity);
+
+  const std::vector<int>& GetBunsAmounts() const;
+  const std::vector<int>& GetVehicles() const;
+  int GetBunsAmount(int town) const;
+  int GetVehicle(int town) const;
+
+  void SetBunsAmounts(std::vector<int> buns_amounts);
+  void SetVehicles(std::vector<int> vehicles);
+  void SetBunsAmount(int town, int buns_amount);
+  void SetVehicle(int town, int vehicle);
+
+  // maybe int64_t as return type
+  int GetTotalBunsAmount() const;
+  int GetTotalVehicles() const;
+
+  int MoveVehicles(int from, int to, int count);
+
+  int Transport(int from, int to, int buns_amount);
+  int TransportWithReturns(int from, int to, int buns_amount);
+};
+
+
