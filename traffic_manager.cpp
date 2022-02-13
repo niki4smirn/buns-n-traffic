8fef437321a1dc653aa89566893d4bdcd25c17d9
niki4smirn <kitos.smirnov@gmail.com>
2022-02-13 19:44:14 +0300

    Add non-total getters implementation

--- traffic_manager.cpp
+++ traffic_manager.cpp
@@ -1,5 +1,7 @@
 #include "traffic_manager.h"
 
+#include <cassert>
+
 TrafficManager::TrafficManager(
     const Graph& graph,
     std::vector<int> buns_amounts,
@@ -9,3 +11,21 @@ TrafficManager::TrafficManager(
     buns_amounts_(std::move(buns_amounts)),
     vehicles_(std::move(vehicles)),
     vehicle_capacity_(vehicle_capacity) {}
+
+const std::vector<int>& TrafficManager::GetBunsAmounts() const {
+  return buns_amounts_;
+}
+
+const std::vector<int>& TrafficManager::GetVehicles() const {
+  return vehicles_;
+}
+
+int TrafficManager::GetBunsAmount(int town) const {
+  assert(town < buns_amounts_.size());
+  return buns_amounts_[town];
+}
+
+int TrafficManager::GetVehicle(int town) const {
+  assert(town < vehicles_.size());
+  return vehicles_[town];
+}
