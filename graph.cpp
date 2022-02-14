b772c71a6419daf35c0d3e4f2692644ec8f984c0
DudkoAndrei <dudkoandrei@gmail.com>
2022-02-14 20:12:14 +0300

    Fix complete graph constructor

--- graph.cpp
+++ graph.cpp
@@ -13,7 +13,7 @@ Graph::Graph(int n) : n_(n) {
 
   for (int i = 0; i < n; ++i) {
     for (int j = 0; j < n; ++j) {
-      if (j != n) {
+      if (j != i) {
         connections_[i].emplace_back(j, 1);
       }
     }
