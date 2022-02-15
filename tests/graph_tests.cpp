787f474548a51c683085b6a859d716771fe1499f
DudkoAndrei <dudkoandrei@gmail.com>
2022-02-15 20:06:38 +0300

    Add test for GetEdgeLength

--- tests/graph_tests.cpp
+++ tests/graph_tests.cpp
@@ -63,3 +63,43 @@ TEST(Graph, GetSize) {
     ASSERT_EQ(graph.GetSize(), 5);
   }
 }
+
+TEST(Graph, GetEdgeLength) {
+  {
+    Graph graph(6);
+
+    for (int i = 0; i < 6; ++i) {
+      for (int j = 0; j < 6; ++j) {
+        if (i == j) {
+          ASSERT_EQ(graph.GetEdgeLength(i, j), 0);
+        } else {
+          ASSERT_EQ(graph.GetEdgeLength(i, j), 1);
+        }
+      }
+    }
+  }
+  {
+    std::vector<std::vector<Graph::Edge>> connections = {
+        {Graph::Edge(4, 6)},
+        {Graph::Edge(2, 3), Graph::Edge(4, 1)},
+        {Graph::Edge(1, 3), Graph::Edge(3, 2)},
+        {Graph::Edge(2, 2), Graph::Edge(4, 7)},
+        {Graph::Edge(0, 6), Graph::Edge(1, 1), Graph::Edge(3, 7)}};
+
+    Graph graph(connections);
+
+    ASSERT_EQ(graph.GetEdgeLength(0, 4), 6);
+    ASSERT_EQ(graph.GetEdgeLength(4, 1), 1);
+    ASSERT_EQ(graph.GetEdgeLength(4, 3), 7);
+    ASSERT_EQ(graph.GetEdgeLength(1, 2), 3);
+    ASSERT_EQ(graph.GetEdgeLength(2, 3), 2);
+    ASSERT_EQ(graph.GetEdgeLength(0, 2), 0);
+
+    ASSERT_EQ(graph.GetEdgeLength(4, 0), 6);
+    ASSERT_EQ(graph.GetEdgeLength(1, 4), 1);
+    ASSERT_EQ(graph.GetEdgeLength(3, 4), 7);
+    ASSERT_EQ(graph.GetEdgeLength(2, 1), 3);
+    ASSERT_EQ(graph.GetEdgeLength(3, 2), 2);
+    ASSERT_EQ(graph.GetEdgeLength(2, 0), 0);
+  }
+}
