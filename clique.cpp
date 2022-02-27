235fdae79d11e92530cb021e033af6216cd4da5b
niki4smirn <kitos.smirnov@gmail.com>
2022-02-27 09:23:29 +0300

    Add Clique interface

---
+++ clique.cpp
@@ -0,0 +1,1 @@
+#include "clique.h"
---
+++ clique.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "abstract_graph.h"
+
+class Clique : public AbstractGraph {
+ public:
+  Clique() = default;
+  explicit Clique(int n);
+  explicit Clique(const std::vector<std::vector<Edge>>& list);
+  explicit Clique(const std::vector<std::vector<int>>& adjacency_matrix);
+
+  const std::vector<Edge>& GetEdges(int from) const override;
+
+  int GetEdgesCount() const override;
+
+  // uses BFS algorithm
+  std::vector<Edge> GetAnyPath(int from, int to) const override;
+  std::vector<Edge> GetShortestPath(int from, int to) const override;
+  std::vector<std::vector<Edge>> GetShortestPaths(int from) const override;
+
+ private:
+  std::vector<std::vector<int>> adjacency_matrix_;
+};
