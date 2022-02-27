03bfbe62a808b7a467f50f81cb1f134606290441
Nikita Smirnov <66160046+niki4smirn@users.noreply.github.com>
2022-02-27 08:10:10 +0300

    Add TrafficManager class (#4)
    * Add TrafficManager interface
    
    * Add TrafficManager constructor implementation
    
    * Add non-total getters implementation
    
    * Add total counters and getters
    
    * Add setters
    
    * Add MoveVehicles implementation and mock method in Graph
    
    * Improve range check asserts
    
    * Add Transport implementation
    
    * Fix CE + GetLenForPath moved to separate method
    
    * Fix buns count bug in Transport
    
    * Add TransportWithReturns implementation
    
    * Add TrafficManager constructor tests
    
    * Add TrafficManager setters tests
    
    * Add TrafficManager getters tests
    
    * Add TrafficManager MoveVehicles tests
    
    * Add TrafficManager Transport tests and fix MoveClosestVehicles bugs
    
    * Add TrafficManager TransportWithReturns tests and fix InitActionsQueue bugs
    
    * Fix cpplint errors
    
    * Fix cpplint errors finally :)

---
+++ traffic_manager.h
@@ -0,0 +1,69 @@
+#pragma once
+
+#include <vector>
+#include <queue>
+
+#include "graph.h"
+
+class TrafficManager {
+ public:
+  TrafficManager(
+      const Graph& graph,
+      std::vector<int> buns_amounts,
+      std::vector<int> vehicles,
+      int vehicle_capacity);
+
+  const std::vector<int>& GetBunsAmounts() const;
+  const std::vector<int>& GetVehicles() const;
+  int GetBunsAmount(int town) const;
+  int GetVehicle(int town) const;
+
+  void SetBunsAmounts(std::vector<int> buns_amounts);
+  void SetVehicles(std::vector<int> vehicles);
+  void SetBunsAmount(int town, int buns_amount);
+  void SetVehicle(int town, int vehicle);
+
+  // maybe int64_t as return type
+  int GetTotalBunsAmount() const;
+  int GetTotalVehicles() const;
+
+  int MoveVehicles(int from, int to, int count);
+
+  int Transport(int from, int to, int buns_amount);
+  int TransportWithReturns(int from, int to, int buns_amount);
+
+ private:
+  friend class TrafficManagerTester;
+
+  void MoveBuns(int from, int to, int count);
+  int MoveClosestVehicles(int to, int count);
+  static int GetLenForPath(const std::vector<Graph::Edge>& path);
+
+  struct ArrivalAction {
+    int timestamp{0};
+    int vehicles_amount{0};
+    int from_town_index{0};
+    int to_town_index{0};
+
+    bool operator>(ArrivalAction arrival_action) const;
+  };
+
+  using ActionsQueue = std::priority_queue<ArrivalAction,
+                                           std::vector<ArrivalAction>,
+                                           std::greater<>>;
+  ActionsQueue InitActionsQueue(
+      int start_town,
+      int finish_town,
+      int main_path_len) const;
+
+ private:
+  Graph graph_;
+  std::vector<int> buns_amounts_;
+  std::vector<int> vehicles_;
+  int vehicle_capacity_{0};
+
+  int total_buns_amount_{0};
+  int total_vehicles_{0};
+};
+
+
