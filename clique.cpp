ede8dfd78b217fd0deb317cfadf87f4277d4da78
niki4smirn <kitos.smirnov@gmail.com>
2022-02-27 09:39:21 +0300

    Add Clique constructors implementation

--- clique.cpp
+++ clique.cpp
@@ -1,1 +1,35 @@
 #include "clique.h"
+
+#include <cassert>
+
+Clique::Clique(int n) : AbstractGraph(n) {
+  adjacency_matrix_.resize(n);
+  for (int i = 0; i < n; ++i) {
+    adjacency_matrix_[i].resize(n);
+    for (int j = 0; j < n; ++j) {
+      if (i != j) {
+        adjacency_matrix_[i][j] = 1;
+      }
+    }
+
+  }
+}
+
+Clique::Clique(const std::vector<std::vector<Edge>>& list) :
+    Clique(list.size()) {
+  for (int i = 0; i < list.size(); ++i) {
+    assert(list[i].size() + 1 == list.size());
+    for (int j = 0; j < list[i].size(); ++j) {
+      auto edge = list[i][j];
+      adjacency_matrix_[i][edge.to] = edge.length;
+    }
+  }
+}
+
+Clique::Clique(const std::vector<std::vector<int>>& adjacency_matrix) :
+    AbstractGraph(adjacency_matrix.size()) {
+  for (int i = 0; i < adjacency_matrix.size(); ++i) {
+    assert(adjacency_matrix[i].size() == adjacency_matrix.size());
+  }
+  adjacency_matrix_ = adjacency_matrix;
+}
